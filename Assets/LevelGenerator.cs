3dbff63c90c4e9680a5b1d9c2d79ac42966f6818
PepoG <peter.gonda.1@gmail.com>
2023-11-25 12:49:14 +0100

    table

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -1,5 +1,21 @@
+using System;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+
+/*
+
+
+
+OOOOOOO
+O.O.O.O
+OP.O.OO
+O B.O.O
+OOOOOOO
+
+
+
+*/
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -7,16 +23,25 @@ public class LevelGenerator : MonoBehaviour
     public GameObject indestructibleBlockPrefab;
     public GameObject playerPrefab;
     public GameObject bombPrefab;
+    public GameObject tablePrefab;
 
     private float cellSize = 1.0f;
-    [Multiline]
+    [TextArea(10, 15)]
     public string levelString = @"
-    OOOOOOO
-    O.O.O.O
-    OP.O.OO
-    O B.O.O
-    OOOOOOO
-    ";
+OOOOOOOOOOOOOOOOOOOOOOO
+O.....................O
+O.O.O.O.O.O.O.O.O.O.O.O
+O.....................O
+O.O.O.O.O.O.O.O.O.O.O.O
+O.....................O
+O.O.O.O.O.O.O.O.O.O.O.O
+O.....................O
+O.O.O.O.O.O.O.O.O.O.O.O
+O.....................O
+OBO.O.O.O.O.O.O.O.O.O.O
+O P...................O
+OOOOOOOOOOOOOOOOOOOOOOO
+";
 
     void Start()
     {
@@ -25,12 +50,19 @@ public class LevelGenerator : MonoBehaviour
 
     void GenerateLevel()
     {
-        string[] rows = levelString.Split('\n');
-        for (int y = 0; y < rows.Length; y++)
+        var rows = levelString.Split(Environment.NewLine).Where(p => p.Length > 0).ToArray();
+
+        var height = rows.Length;
+        var width = rows[0].Length;
+
+        for (int y = 0; y < height; y++)
         {
             string row = rows[y];
-            for (int x = 0; x < row.Length; x++)
+            for (int x = 0; x < width; x++)
             {
+                if (row.Length > width) {
+                    Debug.LogWarning("Row " + row + " has wrong column count.");
+                }
                 char cell = row[x];
                 var position = transform.position + new Vector3(x * cellSize, 0, -y * cellSize);
 
@@ -56,9 +88,13 @@ public class LevelGenerator : MonoBehaviour
                 }
                 else
                 {
-                    Debug.Log("Unknown block" + cell);
+                    Debug.Log("Unknown block '" + cell + "'");
                 }
             }
         }
+
+        var tableGo = Instantiate(tablePrefab, Vector3.zero, Quaternion.identity, transform);
+        tableGo.transform.localScale = new Vector3(width, 0.1f, height);
+        tableGo.transform.localPosition = new Vector3((width - 1) / 2, -1f, (height - 1) / -2);
     }
 }
